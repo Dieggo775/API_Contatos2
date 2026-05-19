e6ec20fd3917361b19b364601f12c514089ea0e8
Diego Araujo <dieggo_taylor@hotmail.com>
2026-05-19 13:33:31 -0300

    Atualização

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 
  const app = express()
+ app.use(express.json())
+
+ const users = []
+
+ app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+    
+    res.send('ok post')
+ })
 
  app.get('/usuarios', (req, res) => {
-    res.send('Ola Mundo')
+    res.json(users)
  })
 
  app.listen(3000)
